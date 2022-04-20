725b3fc14206711666369cc9718a80086cda783c
jogeshgupta <jogeshgupta963@gmail.com>
2022-04-20 13:42:30 +0530

    cas

---
+++ src/components/MovieList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const MovieList = ({movies}) => {
+	
+
+	return (
+		<>
+			{movies.map((movie, index) => (
+				<div  className='d-flex justify-content-start m-3'>
+					<img src={movie.Poster} alt='movie'></img>
+				</div>
+			))}
+		</>
+	);
+};
+
+export default MovieList;
