5b1bf0984c9a54530a7aa3f1e63433f719dec77c
jogeshgupta <jogeshgupta963@gmail.com>
2022-04-20 13:42:17 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MovieList from './components/MovieList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+	const [movies, setMovies] = useState([]);
+    const [searchVal,setSearchVal] = useState('')
+
+    async function getMovieRequest(){
+        const url = `http://www.omdbapi.com/?s=avengers&apikey=36869c0c`
+        var {data} = await axios.get(url)
+        const {Search} = data
+        setMovies(Search)
+    }
+    useEffect(() => {
+        getMovieRequest()
+    }, [])
+	return (
+		<div className='container-fluid movie-app'>
+			<div className='row'>
+                
+            </div>
+            
+			<div className='row'>
+				<MovieList
+					movies={movies}
+				/>
+			</div>
+		</div>
+	);
+};
+
+
 
 export default App;
